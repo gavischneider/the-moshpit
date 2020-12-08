208e89dd4c4e0cddc94faf48b944e33e5ad7d335
Gavi Schneider <gavischneider@gmail.com>
2020-12-08 23:15:45 +0200

    Infinite scroll is mostly done

--- src/client/src/components/Newsfeed.tsx
+++ src/client/src/components/Newsfeed.tsx
@@ -1,3 +1,44 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
+import usePostFetch from "../hooks/usePostFetch";
+import { Post } from "./Post";
 
-export const Newsfeed = () => {};
+export const Newsfeed = () => {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { loading, error, posts, hasMore } = usePostFetch(query, pageNumber);
+
+  const observer: any = useRef();
+  const lastPostElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer && observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          console.log("Visible ");
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+      console.log(node);
+    },
+    [loading, hasMore]
+  );
+
+  return (
+    <div>
+      {posts.map((post, index) => {
+        if (posts.length === index + 1) {
+          return (
+            <div ref={lastPostElementRef} key={post.id}>
+              post.title{" "}
+            </div>
+          );
+        }
+        return <Post key={post.id} name={post.title} />;
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
+    </div>
+  );
+};
--- src/client/src/hooks/usePostFetch.ts
+++ src/client/src/hooks/usePostFetch.ts
@@ -1,11 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+interface Post {
+  title: string;
+  id: string;
+  description: string;
+  url: string;
+  created: string;
+  author: string;
+  category: string[];
+  enclosures: object[];
+  image: string;
+}
+
 // Change the string to type post
 export default function usePostFetch(query: any, pageNumber: number) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [posts, setPosts] = useState(Array<string>());
+  const [posts, setPosts] = useState(Array<Post>());
   const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
@@ -18,13 +30,21 @@ export default function usePostFetch(query: any, pageNumber: number) {
 
     axios({
       method: "GET",
-      url: "BACKEND API",
+      url: "getposts",
       params: { query: query, page: pageNumber },
     })
       .then((res) => {
         console.log(res.data);
-        setPosts((prevPosts) => {
-          return [...new Set([...prevPosts, ...res.data.posts])];
+        setPosts((prevPosts: any) => {
+          const arr: Post[] = [...prevPosts, ...res.data.posts];
+          const map: any = {};
+          for (const post of arr) {
+            map[post.id] = post;
+          }
+          const newArray: Post[] = Object.values(map);
+          return newArray;
+
+          //return [...new Set([...prevPosts, ...res.data.posts])];
         });
         setHasMore(res.data.posts.length > 0);
         setLoading(false);
