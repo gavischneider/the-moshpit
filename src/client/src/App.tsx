216a37eee3b39d92c6204e1f58cc76189aa53be4
Gavi Schneider <gavischneider@gmail.com>
2020-12-10 17:48:00 +0200

    removed white bar

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -6,7 +6,7 @@ import { Newsfeed } from "./components/Newsfeed";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-black">
       <Navbar />
       <Sidebar />
       <Newsfeed />
