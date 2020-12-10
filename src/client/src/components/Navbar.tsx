f6a526b5dbc8a811c0afa5541e59930364b09ea3
Gavi Schneider <gavischneider@gmail.com>
2020-12-10 16:13:49 +0200

    Added new sidebar

--- src/client/src/components/Navbar.tsx
+++ src/client/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-900 border-solid border-b-2 border-red-600 pl-6 pr-6 pt-3 pb-3 shadow-lg inset-x-0 top-0 object-top sticky z-10">
+    <nav className="z-20 flex items-center justify-between flex-wrap bg-gray-900 border-solid border-b-2 border-red-600 pl-6 pr-6 pt-3 pb-3 shadow-lg inset-x-0 top-0 object-top sticky">
       <div className="flex">
         <div className="my-auto pr-1"></div>
       </div>
