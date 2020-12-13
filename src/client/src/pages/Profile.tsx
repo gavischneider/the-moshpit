80941803829c22fee6776e4fc059e4d198bcf337
Gavi Schneider <gavischneider@gmail.com>
2020-12-13 22:08:21 +0200

    Added basic profile info to page

--- src/client/src/pages/Profile.tsx
+++ src/client/src/pages/Profile.tsx
@@ -1,21 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../components/Navbar";
+import { User } from "../../../shared/User";
 import axios from "axios";
 
 export const Profile = () => {
+  const [profile, setProfile] = useState(Array<User>());
+  const proxyUrl = "http://localhost:5000";
+
   useEffect(() => {
     axios({
       method: "GET",
       url: "auth/user",
     }).then((res: any) => {
-      console.log(res);
+      console.log(res.data);
+      setProfile([res.data]);
     });
   }, []);
+
+  console.log(`PROFILE: ${profile}`);
+  if (!profile[0]) return <span>loading...</span>;
 
   return (
     <div>
       <Navbar />
       <h1>Profile!!!</h1>
+      <h2>{profile[0].username}</h2>
+      <h2>{profile[0].email}</h2>
+      <img src={profile[0].photo} alt={"profile"} />
     </div>
   );
 };
