50d5612ec69a28d2c4626c8224500a96e0362978
Gavi Schneider <gavischneider@gmail.com>
2021-01-19 16:50:52 +0200

    Added styles to login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,15 +2,24 @@ import React from "react";
 import { Navbar } from "../components/Navbar";
 
 export const Login = () => {
+  const imgSource = "google_signin.png";
   return (
-    <div>
+    <div className="bg-gray-900 min-h-screen">
       <Navbar />
-      <h1 className="text-3xl mb-3">Login</h1>
-      <ul className="flex">
-        <li>
-          <a href="http://localhost:5000/auth/google">Google</a>
-        </li>
-      </ul>
+      <div id="content" className="flex justify-center pt-8">
+        <div>
+          <h1 className="text-3xl mb-3 text-white flex justify-center">
+            Sign In
+          </h1>
+          <ul className="flex">
+            <li>
+              <a href="http://localhost:5000/auth/google">
+                <img src={imgSource} className="" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
